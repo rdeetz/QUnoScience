c5464476b6f3e530fc6259a1dc1466cd372d7cc0
Roger Deetz <rdeetz@gmail.com>
2022-02-19 22:34:13 -0500

    Added command line parser.

--- QUnoDoctor/Program.cs
+++ QUnoDoctor/Program.cs
@@ -2,4 +2,28 @@
 //   Copyright (c) 2022 Roger Deetz. All rights reserved.
 // </copyright>
 
-Console.WriteLine("Hello, World!");
+using System.CommandLine;
+using System.IO;
+
+var fileInOption = new Option<FileInfo>(
+    "--input-file",
+    "The raw data file to process.");
+
+var fileOutOption = new Option<FileInfo>(
+    "--output-file",
+    "The processed file.");
+
+var rootCommand = new RootCommand { fileInOption, fileOutOption };
+rootCommand.Description = "An Uno-like card game.";
+
+rootCommand.SetHandler(
+    (FileInfo fileIn, FileInfo fileOut) => 
+    {
+        Console.WriteLine($"The input file is: {fileIn?.FullName ?? null}");
+        Console.WriteLine($"The output file is: {fileOut?.FullName ?? null}");
+    },
+    fileInOption, 
+    fileOutOption
+);
+
+rootCommand.Invoke(args);
