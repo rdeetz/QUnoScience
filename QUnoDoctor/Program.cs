f3c745a728e99fed2cdab3ba152feb7617bf330b
Roger Deetz <rdeetz@gmail.com>
2022-04-19 07:04:33 -0400

    Renamed some variables.

--- QUnoDoctor/Program.cs
+++ QUnoDoctor/Program.cs
@@ -8,28 +8,27 @@ using System.IO;
 FileInfo? inputFile = null;
 FileInfo? outputFile = null;
 
-var fileInOption = new Option<FileInfo>(
+var inputFileOption = new Option<FileInfo>(
     "--input-file",
     "The raw data file to process.");
 
-var fileOutOption = new Option<FileInfo>(
+var outputFileOption = new Option<FileInfo>(
     "--output-file",
     "The processed file.");
 
-var rootCommand = new RootCommand { fileInOption, fileOutOption };
+var rootCommand = new RootCommand { inputFileOption, outputFileOption };
 rootCommand.Description = "An Uno-like card game.";
 
 rootCommand.SetHandler(
     (FileInfo fileIn, FileInfo fileOut) => 
     {
-        Console.WriteLine($"The input file is: {fileIn?.FullName ?? null}");
-        Console.WriteLine($"The output file is: {fileOut?.FullName ?? null}");
-
-        if (fileIn != null) inputFile = fileIn;
-        if (fileOut != null) outputFile = fileOut;
+        Console.WriteLine($"The input file is: {fileIn?.FullName ?? String.Empty}");
+        Console.WriteLine($"The output file is: {fileOut?.FullName ?? String.Empty}");
+        inputFile ??= fileIn;
+        outputFile ??= fileOut;
     },
-    fileInOption, 
-    fileOutOption
+    inputFileOption, 
+    outputFileOption
 );
 
 rootCommand.Invoke(args);
